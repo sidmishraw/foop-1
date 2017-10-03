c08e684b093f80eb145d9924a2935bc1e816da7b
Sidharth Mishra <sidmishraw@gmail.com>
2017-10-03 16:44:13 -0700

    Added bank test example

---
+++ src/test/java/foop/test/bank/BankDriver.java
@@ -0,0 +1,29 @@
+/**
+ * Project: Foops_1
+ * Package: foop.test.bank
+ * File: BankDriver.java
+ * 
+ * @author sidmishraw
+ *         Last modified: Oct 3, 2017 4:35:05 PM
+ */
+package foop.test.bank;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author sidmishraw
+ *
+ *         Qualified Name: foop.test.bank.BankDriver
+ *
+ */
+public class BankDriver {
+    
+    private static final Logger logger = LoggerFactory.getLogger(BankDriver.class);
+    
+    @Test
+    public void driverInitiate() {
+        
+    }
+}
